a404e12373eb4418f4c4410f094200b65f6c5aa2
Artem_Korchebny <artem9n24@yandex.ru>
2021-09-17 12:18:47 +0800

    yandex pay: refactoring - change include sdk js yandex pay

--- include.php
+++ include.php
@@ -1,13 +1,9 @@
 <?php
 
 $jsConfig = [
-	'yandex_pay_sdk' => [
+	'yandexpay.sdk' => [
 		'js' => 'https://pay.yandex.ru/sdk/v1/pay.js'
 	],
-	'yandex_pay_load' => [
-		'js' => BX_ROOT . '/js/yandexpay.pay/yandexpayload.js',
-		'css' => BX_ROOT . '/css/yandexpay.pay/yandexpay.css',
-	]
 ];
 
 foreach ($jsConfig as $ext => $arExt)
