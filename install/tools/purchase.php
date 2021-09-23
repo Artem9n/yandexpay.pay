5717e613749d1fa4f9ccde65819db34bd39aca3d
Artem_Korchebny <artem9n24@yandex.ru>
2021-09-23 15:57:14 +0800

    yandex pay: trading - url for process request

---
+++ install/tools/purchase.php
@@ -0,0 +1,14 @@
+<?php
+
+define('STOP_STATISTICS', true);
+define('NO_AGENT_CHECK', true);
+define('NOT_CHECK_PERMISSIONS', true);
+define('DisableEventsCheck', true);
+
+require $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php';
+
+global $APPLICATION;
+
+$APPLICATION->IncludeComponent('yandexpay.pay:trading.cart', '');
+
+require $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/epilog_after.php';
