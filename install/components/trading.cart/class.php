5bbf1ffcdd1cad74d52f9ffc5850096b8858da18
Artem_Korchebny <artem9n24@yandex.ru>
2021-10-21 17:27:51 +0800

    #193215 yandex pay order: change component trading cart

--- install/components/trading.cart/class.php
+++ install/components/trading.cart/class.php
@@ -3,11 +3,14 @@
 namespace YandexPay\Pay\Components;
 
 use Bitrix\Main;
+use Bitrix\Sale;
+use Bitrix\Iblock\ElementTable;
 use Bitrix\Main\Localization\Loc;
-use Bitrix\Sale\PaySystem;
-use Sale\Handlers\PaySystem\YandexPayHandler;
 use YandexPay\Pay\Reference\Assert;
-use YandexPay\Pay\Utils\JsonBodyFilter;
+use YandexPay\Pay\Trading\Setup;
+use YandexPay\Pay\Trading\Entity\Reference as EntityReference;
+use YandexPay\Pay\Trading\Entity\Registry as EntityRegistry;
+use YandexPay\Pay\Utils;
 
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) { die(); };
 
@@ -15,15 +18,20 @@ Loc::loadMessages(__FILE__);
 
 class TradingCart extends \CBitrixComponent
 {
-	/** @var PaySystem\BaseServiceHandler */
+	/** @var Sale\PaySystem\BaseServiceHandler */
 	protected $handler;
-	/** @var PaySystem\Service */
+	/** @var Sale\PaySystem\Service */
 	protected $service;
+	/** @var EntityReference\Environment */
+	protected $environment;
+	/** @var Setup\Model */
+	protected $setup;
 
 	public function onPrepareComponentParams($arParams) : array
 	{
 		$arParams['PRODUCT_ID'] = !empty($arParams['PRODUCT_ID']) ? (int)$arParams['PRODUCT_ID'] : null;
 		$arParams['PAY_SYSTEM_ID'] = !empty($arParams['PAY_SYSTEM_ID']) ? (int)$arParams['PAY_SYSTEM_ID'] : null;
+		$arParams['SETUP_ID'] = !empty($arParams['SETUP_ID']) ? (int)$arParams['SETUP_ID'] : null;
 
 		return $arParams;
 	}
@@ -32,11 +40,16 @@ class TradingCart extends \CBitrixComponent
 	{
 		try
 		{
-
 			$this->loadModules();
-			$handler = $this->getHandler();
+			$this->bootstrap();
 
+			$handler = $this->getHandler();
+			if ($handler === null) { return; }
+
+			$this->setParameters($handler);
 			pr($handler->getParamsBusValue());
+
+			$this->includeComponentTemplate();
 		}
 		catch (Main\SystemException $exception)
 		{
@@ -44,7 +57,117 @@ class TradingCart extends \CBitrixComponent
 		}
 	}
 
-	protected function getHandler() : ?PaySystem\BaseServiceHandler
+	protected function setParameters(Sale\PaySystem\BaseServiceHandler $handler) : void
+	{
+		global $USER;
+
+		$params = $handler->getParamsBusValue();
+
+		$setup = $this->getSetup();
+		$setup->wakeupOptions();
+		$setup->fillSiteId();
+		$options = $setup->getOptions();
+
+		$product = $this->getProduct();
+
+		$gataway = $this->service->getField('PS_MODE');
+
+		$this->arResult['PARAMS'] = [
+			'env'               => $params['YANDEX_PAY_TEST_MODE'] === 'Y' ? 'SANDBOX' : 'PRODUCTION',
+			'merchantId'        => $params['YANDEX_PAY_MERCHANT_ID'],
+			'merchantName'      => $params['YANDEX_PAY_MERCHANT_NAME'],
+			'buttonTheme'       => $params['YANDEX_PAY_VARIANT_BUTTON'],
+			'buttonWidth'       => $params['YANDEX_PAY_WIDTH_BUTTON'],
+			'cardNetworks'      => $this->getCardNetworks($params),
+			'gateway'           => $gataway,
+			'gatewayMerchantId' => $params['YANDEX_PAY_' . $gataway . '_PAYMENT_GATEWAY_MERCHANT_ID'],
+			'useEmail'          => (bool)$options->getValue('USE_BUYER_EMAIL'),
+			'useName'           => (bool)$options->getValue('USE_BUYER_NAME'),
+			'usePhone'          => (bool)$options->getValue('USE_BUYER_PHONE'),
+			'purchaseUrl'       => $options->getValue('PURCHASE_URL'),
+			'siteUrl'           => Utils\Url::absolutizePath(),
+			'productId'         => $product['ID'],
+			'siteId'            => $setup->getSiteId(),
+			'userId'            => $USER->GetID(),
+			'fUserId'           => Sale\Fuser::getId(true),
+			'order'             => [
+				'id' => '0',
+				'total' => number_format($product['PRICE'], 2, '.', ''),
+				'items' => [
+					[
+						'label' => $product['NAME'],
+						'amount' => number_format($product['PRICE'], 2, '.', '')
+					]
+				]
+			]
+		];
+	}
+
+	protected function getProduct() : array
+	{
+		$result = [];
+
+		if ($this->arParams['PRODUCT_ID'] === null || $this->arParams['PRODUCT_ID'] <= 0 ) { return $result; }
+
+		$query = ElementTable::getList([
+			'filter' => [
+				'=ID' => $this->arParams['PRODUCT_ID']
+			],
+			'select' => ['ID', 'IBLOCK_ID', 'NAME'],
+			'limit' => 1
+		]);
+
+		if ($row = $query->fetch())
+		{
+			$price = \CCatalogProduct::GetOptimalPrice($row['ID']);
+
+			$result = [
+				'ID' => $row['ID'],
+				'NAME' => $row['NAME'],
+				'PRICE' => $price['DISCOUNT_PRICE']
+			];
+		}
+
+		return $result;
+	}
+
+	protected function getSetup() : Setup\Model
+	{
+		if ($this->setup === null)
+		{
+			$this->setup = $this->loadSetup();
+		}
+
+		return $this->setup;
+	}
+
+	protected function loadSetup() : Setup\Model
+	{
+		return Setup\Model::wakeUp(['ID' => $this->arParams['SETUP_ID']]);
+	}
+
+	protected function getCardNetworks(array $parameters) : array
+	{
+		$result = [];
+
+		$str = 'YANDEX_CARD_NETWORK_';
+		$strLength = mb_strlen($str);
+
+		foreach ($parameters as $code => $value)
+		{
+			$position = mb_strpos($code, $str);
+
+			if ($position !== false && $value === 'Y')
+			{
+				$cardName = mb_substr($code, $strLength);
+				$result[] = $cardName;
+			}
+		}
+
+		return $result;
+	}
+
+	protected function getHandler() : ?Sale\PaySystem\BaseServiceHandler
 	{
 		if ($this->handler === null)
 		{
@@ -58,16 +181,16 @@ class TradingCart extends \CBitrixComponent
 	{
 		$service = $this->getService();
 
-		[$className, $handlerType] = PaySystem\Manager::includeHandler($service->getField('ACTION_FILE'));
+		[$className, $handlerType] = Sale\PaySystem\Manager::includeHandler($service->getField('ACTION_FILE'));
 
 		$this->handler = new $className($handlerType, $service);
 
-		Assert::typeOf($this->handler, YandexPayHandler::class, 'handler');
+		Assert::typeOf($this->handler, \Sale\Handlers\PaySystem\YandexPayHandler::class, 'handler');
 
 		return $this->handler;
 	}
 
-	protected function getService() : PaySystem\Service
+	protected function getService() : Sale\PaySystem\Service
 	{
 		if ($this->service === null)
 		{
@@ -77,7 +200,7 @@ class TradingCart extends \CBitrixComponent
 		return $this->service;
 	}
 
-	protected function loadService() : ?PaySystem\Service
+	protected function loadService() : ?Sale\PaySystem\Service
 	{
 		if ($this->arParams['PAY_SYSTEM_ID'] === null || $this->arParams['PAY_SYSTEM_ID'] <= 0 )
 		{
@@ -86,7 +209,7 @@ class TradingCart extends \CBitrixComponent
 
 		$result = null;
 
-		$query = PaySystem\Manager::getList([
+		$query = Sale\PaySystem\Manager::getList([
 			'filter' => [
 				'=ID' => $this->arParams['PAY_SYSTEM_ID'],
 				'=ACTION_FILE' => 'yandexpay',
@@ -97,15 +220,10 @@ class TradingCart extends \CBitrixComponent
 
 		if ($item = $query->fetch())
 		{
-			$result = new PaySystem\Service($item);
+			$result = new Sale\PaySystem\Service($item);
 		}
 
 		return $result;
-	}
-
-	protected function parseRequest() : void
-	{
-		$this->request->addFilter(new JsonBodyFilter());
 	}
 
 	protected function loadModules(): void
@@ -129,6 +247,11 @@ class TradingCart extends \CBitrixComponent
 			'yandexpay.pay',
 			'sale'
 		];
+	}
+
+	protected function bootstrap() : void
+	{
+		$this->environment = EntityRegistry::getEnvironment();
 	}
 
 	protected function getLang(string $code, $replace = null, $language = null): string
--- install/components/trading.cart/templates/.default.php
+++ install/components/trading.cart/templates/.default.php
@@ -2,8 +2,7 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) { die(); }
 
 /**
- * @var array $params
- * @var $this \Sale\Handlers\PaySystem\YandexPayHandler
+ * @var array $arResult
  */
 
 use Bitrix\Main\Localization\Loc;
@@ -14,31 +13,13 @@ Loc::loadMessages(__FILE__);
 
 echo Extension::getHtml('yandexpaypay.widget');
 
-$widgetOptions = array_intersect_key($params, [
-	'requestSign' => true,
-	'env' => true,
-	'merchantId' => true,
-	'merchantName' => true,
-	'buttonTheme' => true,
-	'buttonWidth' => true,
-	'gateway' => true,
-	'gatewayMerchantId' => true,
-	'externalId' => true,
-	'paySystemId' => true,
-	'currency' => true,
-	'YANDEX_PAY_NOTIFY_URL' => true,
-]);
-
 ?>
-<div>
-	<?= Loc::getMessage('YANDEX_MARKET_SALE_SUM_DESCRIPTION', ['#SUM#' => CurrencyFormat($params['order']['total'], $params['currency'])])?>
-</div>
 <div id="yandexpay" class="yandex-pay"></div>
 <script>
 	(function() {
 		const element = document.getElementById('yandexpay');
-		const widget = new BX.YandexPay.Widget(element, <?= Json::encode($widgetOptions) ?>);
+		const widget = new BX.YandexPay.Widget(element, <?= Json::encode($arResult['PARAMS']) ?>);
 
-		widget.cart(<?= Json::encode($params['order']) ?>);
+		widget.cart(<?= Json::encode($arResult['PARAMS']['order']) ?>);
 	})();
 </script>
