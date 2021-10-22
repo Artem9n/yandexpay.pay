1ad333db196d0429293cb9aca7407ca9e088eb57
dolphin <ae@b2bit.ru>
2021-10-22 16:57:54 +0800

    #193215 yandex pay: add gateway rbs

---
+++ lang/ru/lib/gateway/payment/alfabank.php
@@ -0,0 +1,18 @@
+<?php
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_DESCRIPTION'] = '<a target="_blank" href="https://alfabank.ru/">RBS</a> – международная платежная платформа, </br>предоставляющая услуги для онлайн-компаний и частных лиц в более чем 60 странах.';
+
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_MERCHANT_ID'] = 'Идентификатор продавца';
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_USERNAME'] = 'Логин учетной записи';
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_MERCHANT_PASSWORD'] = 'Пароль учетной записи';
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_MERCHANT_PASSWORD_DESCRIPTION'] = 'Пароль полученый от банка для API запросов';
+
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_ERROR_0'] ='Заказ зарегистрирован, но не оплачен';
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_ERROR_1'] ='Предавторизованная сумма захолдирована (для двухстадийных платежей)';
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_ERROR_2'] ='Проведена полная авторизация суммы заказа;';
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_ERROR_3'] ='3DS Авторизация отменена';
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_ERROR_4'] ='По транзакции была проведена операция возврата';
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_ERROR_5'] ='Инициирована авторизация через ACS банка-эмитента';
+$MESS['YANDEX_PAY_GATEWAY_PAYMENT_ALFABANK_ERROR_6'] ='3DS Авторизация отклонена';
+
+
+?>
