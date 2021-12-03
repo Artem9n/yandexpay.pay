917703ca6c34ca062f7b67ae522e3775930b34dc
Artem_Korchebny <artem9n24@yandex.ru>
2021-12-03 18:25:01 +0800

    #193215 yandex pay order: events behavior

---
+++ lib/injection/engine/element.php
@@ -0,0 +1,120 @@
+<?php
+namespace YandexPay\Pay\Injection\Engine;
+
+use Bitrix\Iblock\ElementTable;
+use Bitrix\Iblock\IblockTable;
+use Bitrix\Main\Context;
+use YandexPay\Pay\Injection;
+use YandexPay\Pay\Reference\Event;
+
+class Element extends Event\Base
+{
+	public static function onEpilog(int $injectionId, array $settings) : void
+	{
+		self::render($injectionId, $settings);
+	}
+
+	protected static function render(int $injectionId, array $settings) : void
+	{
+		if (self::$handlerDisallowYaPay) { return; }
+
+		global $APPLICATION;
+
+		$model = Injection\Setup\Model::wakeUp(['ID' => $injectionId]);
+		$model->fillBehavior();
+
+		if ($model->getBehavior() !== Injection\Behavior\Registry::ELEMENT) { return; }
+
+		if (trim($model->getSelectorValue()) === '') { return; }
+
+		$iblockId = $settings['IBLOCK'];
+
+		$url = static::getUrl();
+		$element = static::getDetailPageUtlTemplate($iblockId, $url);
+
+		if (empty($element)) { return; }
+
+		$elementId = static::getElementId($iblockId, $element);
+
+		if ($elementId === null) { return; }
+
+		$APPLICATION->IncludeComponent('yandexpay.pay:trading.cart', '', [
+			'INJECTION_ID'	=> $injectionId,
+			'PRODUCT_ID'	=> $elementId,
+			'MODE'			=> Injection\Behavior\Registry::ELEMENT
+		], false);
+
+		self::$handlerDisallowYaPay = true;
+	}
+
+	protected static function getUrl() : string
+	{
+		return Context::getCurrent()->getRequest()->getRequestedPage();
+	}
+
+	protected static function getDetailPageUtlTemplate(int $iblockId, string $url) : ?array
+	{
+		$result = null;
+		$variables = [];
+
+		$engine = new \CComponentEngine();
+
+		$query = IblockTable::getList([
+			'filter' => [ '=ID' => $iblockId ],
+			//'cache' => [ 'ttl' => 360000 ], //todo, not clear
+		]);
+
+		$iblock = $query->fetchObject();
+
+		if (!$iblock) { return null; }
+
+		$result['SEF_FOLDER'] = mb_substr($iblock->getListPageUrl(), mb_strlen('#SITE_DIR#'));
+		$result['SEF_URL_TEMPLATES']['element'] = mb_substr($iblock->getDetailPageUrl(), mb_strlen($iblock->getListPageUrl()));
+
+		$engine->guessComponentPath(
+			$result['SEF_FOLDER'],
+			$result['SEF_URL_TEMPLATES'],
+			$variables,
+			$url
+		);
+
+		if (!empty($variables)) { return $variables; }
+
+		$componentVariables = [
+			'SECTION_ID',
+			'SECTION_CODE',
+			'ELEMENT_ID',
+			'ELEMENT_CODE'
+		];
+
+		\CComponentEngine::initComponentVariables(false, $componentVariables, [], $variables);
+
+		return $variables;
+	}
+
+	protected static function getElementId(int $iblockId, array $element) : ?int
+	{
+		$filter = ['ACTIVE' => 'Y', 'IBLOCK_ID' => $iblockId];
+
+		if (isset($element['ELEMENT_CODE']))
+		{
+			$filter['=CODE'] = $element['ELEMENT_CODE'];
+		}
+
+		if (isset($element['ELEMENT_ID']))
+		{
+			$filter['=ID'] = $element['ELEMENT_ID'];
+		}
+
+		$query = ElementTable::getList([
+			'filter' => $filter,
+			'select' => ['ID']
+		]);
+
+		$row = $query->fetchObject();
+
+		if (!$row) { return null; }
+
+		return $row->getId();
+	}
+}
---
+++ lib/injection/engine/order.php
@@ -0,0 +1,69 @@
+<?php
+namespace YandexPay\Pay\Injection\Engine;
+
+use Bitrix\Main\Context;
+use YandexPay\Pay\Reference\Event;
+use YandexPay\Pay\Injection;
+
+class Order extends Event\Base
+{
+	public static function onEpilog(int $injectionId, array $settings) : void
+	{
+		self::render($injectionId, $settings);
+	}
+
+	protected static function render(int $injectionId, array $settings) : void
+	{
+		if (self::$handlerDisallowYaPay) { return; }
+
+		global $APPLICATION;
+
+		$model = Injection\Setup\Model::wakeUp(['ID' => $injectionId]);
+		$model->fillBehavior();
+
+		if ($model->getBehavior() !== Injection\Behavior\Registry::ORDER) { return; }
+
+		if (!static::isOrderPath($settings['PATH_ORDER'])) { return; }
+
+		if (trim($model->getSelectorValue()) === '') { return; }
+
+		$APPLICATION->IncludeComponent('yandexpay.pay:trading.cart', '', [
+			'INJECTION_ID'	=> $injectionId,
+			'MODE'			=> Injection\Behavior\Registry::ORDER
+		], false);
+
+		self::$handlerDisallowYaPay = true;
+	}
+
+	protected static function getUrl() : string
+	{
+		return Context::getCurrent()->getRequest()->getRequestUri();
+	}
+
+	protected static function isOrderPath(string $basketPath) : bool
+	{
+		$result = false;
+
+		$url = static::getUrl();
+
+		if ($url === $basketPath) { return true; }
+
+		$url = static::normalize($url);
+
+		if ($basketPath === $url) { return true; }
+
+		return $result;
+	}
+
+	protected static function normalize($path) : string
+	{
+		$symbolPos = mb_strpos($path, '?');
+
+		if ($symbolPos !== false)
+		{
+			$path = mb_substr($path, 0, $symbolPos);
+		}
+
+		return $path;
+	}
+}
