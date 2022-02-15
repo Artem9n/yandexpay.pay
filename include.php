335422a8d26dda5a6ddb2c72694504a4174fcd14
Mishakov Petr <ceri37@yandex.ru>
2022-02-15 10:37:52 +0800

    #216430 purchase rest: jwt library

--- include.php
+++ include.php
@@ -1,5 +1,10 @@
 <?php
 
+use Bitrix\Main\ModuleManager;
+use Bitrix\Main\Loader;
+
+// js extension
+
 $jsConfig = [
 	'yandexpay.sdk' => [
 		'js' => 'https://pay.yandex.ru/sdk/v1/pay.js'
@@ -9,4 +14,15 @@ $jsConfig = [
 foreach ($jsConfig as $ext => $arExt)
 {
 	\CJSCore::RegisterExt($ext, $arExt);
+}
+
+// vendor
+
+if (CheckVersion(ModuleManager::getVersion('main'), '21.0.0'))
+{
+	Loader::registerNamespace('Firebase\\JWT', __DIR__ . '/vendor/firebase/php-jwt/src');
+}
+else
+{
+	require_once __DIR__ . '/vendor/autoload.php';
 }
