f9ae2a781f78ca3882799df6000089c525a52928
Korchebny <artem9n24@yandex.ru>
2022-03-02 10:28:27 +0800

    #218720 added lang files for behavior group, fast view element

--- lang/ru/lib/injection/behavior/basket.php
+++ lang/ru/lib/injection/behavior/basket.php
@@ -3,3 +3,5 @@
 $MESS['YANDEX_PAY_INJECTION_BEHAVIOR_BASKET_TITLE'] = 'Корзина товаров';
 $MESS['YANDEX_PAY_INJECTION_BEHAVIOR_BASKET_SELECTOR'] = 'Селектор, куда привязывать кнопку "Yandex Pay"';
 $MESS['YANDEX_PAY_INJECTION_BEHAVIOR_BASKET_PATH'] = 'Путь к странице с корзиной от корня сайта';
+
+$MESS['YANDEX_PAY_INJECTION_BEHAVIOR_BASKET_VIEW'] = 'Настройки "Отображения"';
--- lang/ru/lib/injection/behavior/order.php
+++ lang/ru/lib/injection/behavior/order.php
@@ -3,3 +3,5 @@
 $MESS['YANDEX_PAY_INJECTION_BEHAVIOR_ORDER_TITLE'] = 'Оформление заказа';
 $MESS['YANDEX_PAY_INJECTION_BEHAVIOR_ORDER_SELECTOR'] = 'Селектор, куда привязывать кнопку "Yandex Pay"';
 $MESS['YANDEX_PAY_INJECTION_BEHAVIOR_ORDER_PATH'] = 'Путь к странице с оформлением заказа от корня сайта';
+
+$MESS['YANDEX_PAY_INJECTION_BEHAVIOR_ORDER_VIEW'] = 'Настройки "Отображения"';
