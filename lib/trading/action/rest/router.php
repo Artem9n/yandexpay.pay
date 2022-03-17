99ffa24acdb915b3bed84918cbf778f8b75420a8
Korchebny <artem9n24@yandex.ru>
2022-03-17 10:36:17 +0800

    #216430 purchase rest: router rest

---
+++ lib/trading/action/rest/router.php
@@ -0,0 +1,32 @@
+<?php
+namespace YandexPay\Pay\Trading\Action\Rest;
+
+use YandexPay\Pay\Reference\Assert;
+
+class Router
+{
+	public function getAction($path) : Reference\HttpAction
+	{
+		$map = $this->actionMap();
+
+		if (!isset($map[$path])) { throw new Exceptions\ActionNotImplemented(); }
+
+		$className = $map[$path];
+
+		Assert::isSubclassOf($className, Reference\HttpAction::class);
+
+		return new $className();
+	}
+
+	/** @return array<string, class-string<Reference\HttpAction>> */
+	protected function actionMap() : array
+	{
+		return [
+			'root' => Root\Action::class,
+			'v1/order/render' => OrderRender\Action::class,
+			'v1/pickup-options' => PickupOptions\Action::class,
+			'v1/pickup-option-details' => PickupDetail\Action::class,
+			'v1/order/create' => OrderCreate\Action::class,
+		];
+	}
+}
