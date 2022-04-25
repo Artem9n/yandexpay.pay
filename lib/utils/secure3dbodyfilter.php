8919918911b8c9caa6eabd13167ad59870d71e2f
Korchebny <artem9n24@yandex.ru>
2022-04-25 11:11:48 +0800

    #216430 purchase rest: fix secure body filter

--- lib/utils/secure3dbodyfilter.php
+++ lib/utils/secure3dbodyfilter.php
@@ -3,19 +3,12 @@
 namespace YandexPay\Pay\Utils;
 
 use Bitrix\Main;
-use YandexPay\Pay\Gateway\Base;
 
 class Secure3dBodyFilter implements Main\Type\IRequestFilter
 {
 	public function filter(array $values) : array
 	{
 		$result = $values;
-		/*$secure = $values['get']['secure3ds'] ?? $values['post']['secure3ds'];
-
-		if (isset($secure))
-		{
-			$result['post'] += Base::parseParams($secure);
-		}*/
 
 		return $result;
 	}
