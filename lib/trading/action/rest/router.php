c62fab9a7cf9d2eab7ae1f3cac0f19ff24bdb47a
Korchebny <artem9n24@yandex.ru>
2022-04-25 11:09:46 +0800

    #216430 purchase rest: add webhook rout, authorize

--- lib/trading/action/rest/router.php
+++ lib/trading/action/rest/router.php
@@ -27,6 +27,8 @@ class Router
 			'v1/pickup-options' => PickupOptions\Action::class,
 			'v1/pickup-option-details' => PickupDetail\Action::class,
 			'v1/order/create' => OrderCreate\Action::class,
+			'v1/webhook' => OrderPayment\Action::class,
+			'authorize' => Authorize\Action::class,
 			'button/data' => ButtonData\Action::class,
 		];
 	}
