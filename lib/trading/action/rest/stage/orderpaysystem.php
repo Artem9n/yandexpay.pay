039e72ffec06b90fd635f135e6c9f97d0cddc5c5
Korchebny <artem9n24@yandex.ru>
2022-05-18 16:39:56 +0800

    #216430 purchase rest: stage pay system rest

---
+++ lib/trading/action/rest/stage/orderpaysystem.php
@@ -0,0 +1,22 @@
+<?php
+namespace YandexPay\Pay\Trading\Action\Rest\Stage;
+
+use YandexPay\Pay\Trading\Action\Rest\State;
+
+class OrderPaySystem
+{
+	public function __invoke(State\OrderCalculation $state)
+	{
+		$this->fillPaySystem($state);
+	}
+
+	protected function fillPaySystem(State\OrderCalculation $state) : void
+	{
+		$paySystemId = $state->options->getPaymentCard();
+
+		if ((int)$paySystemId > 0)
+		{
+			$state->order->createPayment($paySystemId);
+		}
+	}
+}
