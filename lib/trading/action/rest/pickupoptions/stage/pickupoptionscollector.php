ba80340f728829a0973ea16cf6c3edc648b97395
Korchebny <artem9n24@yandex.ru>
2022-05-18 16:38:52 +0800

    #216430 purchase rest: change pickup options collect

--- lib/trading/action/rest/pickupoptions/stage/pickupoptionscollector.php
+++ lib/trading/action/rest/pickupoptions/stage/pickupoptionscollector.php
@@ -5,7 +5,6 @@ use YandexPay\Pay\Trading\Action\Rest\Reference;
 use YandexPay\Pay\Trading\Action\Rest\Stage;
 use YandexPay\Pay\Trading\Action\Rest\State;
 use YandexPay\Pay\Trading\Entity\Sale as EntitySale;
-use YandexPay\Pay\Trading\Entity\Reference as EntityReference;
 
 class PickupOptionsCollector extends Stage\OrderDeliveryCollector
 {
@@ -30,18 +29,12 @@ class PickupOptionsCollector extends Stage\OrderDeliveryCollector
 			if (!$state->environment->getDelivery()->isCompatible($deliveryId, $state->order)) { continue; }
 
 			$allStores = $state->environment->getDelivery()->getPickupStores($deliveryId, $state->order, $this->bounds);
-			//$storesByLocation = $this->groupStoresByLocation($allStores); //todo, need group?
+
 			foreach ($allStores as $locationId => $stores)
 			{
-				$state->order->setLocation($locationId);
-
-				$calculationResult = $state->environment->getDelivery()->calculate($deliveryId, $state->order);
-
-				if (!$calculationResult->isSuccess()) { continue; }
-
 				foreach ($stores as $store)
 				{
-					$result[] = $this->collectPickupOption($calculationResult, $store, $locationId);
+					$result[] = $this->collectPickupOption($deliveryId, $store, $locationId);
 				}
 			}
 		}
@@ -49,28 +42,19 @@ class PickupOptionsCollector extends Stage\OrderDeliveryCollector
 		$this->write($result);
 	}
 
-	protected function collectPickupOption(EntityReference\Delivery\CalculationResult $calculationResult, array $store, int $locationId = null) : array
+	protected function collectPickupOption(int $deliveryId, array $store, int $locationId = null) : array
 	{
 		return [
-			'pickupPointId' => implode(':', [$calculationResult->getDeliveryId(), $store['ID'], $locationId]), //todo send need metadata
-			'provider' => ['IN_STORE'], //todo enum<PICKPOINT|...|IN_STORE>
+			'pickupPointId' => implode(':', [$deliveryId, $store['ID'], $locationId]), //todo send need metadata
+			'provider' => 'IN_STORE',
 			'address' => $store['ADDRESS'],
 			'location' =>  [
 				'latitude' => (float)$store['GPS_N'],
 				'longitude' => (float)$store['GPS_S'],
 			],
 			'title'     => $store['TITLE'],
-			'fromDate' => $calculationResult->getDateFrom()->format('Y-m-d'),
-			'toDate' => $calculationResult->getDateTo()->format('Y-m-d'),
-			'amount'    => (float)$calculationResult->getPrice(),
 			'description' => $store['DESCRIPTION'],
 			'phones' => explode(', ', $store['PHONE']),
-			'schedule' =>  [ // Опционально. График работы точки.
-				'label' => '', //todo # Например, "пн-пт"
-				'fromTime' => '', //todo # HH:mm, "08:00"
-				'toTime' => '', //todo # HH:mm, "20:00"
-			],
-			'storagePeriod' => 0, // todo # Опционально. Срок хранения товара в точке самовывоза в днях.
 		];
 	}
 }
