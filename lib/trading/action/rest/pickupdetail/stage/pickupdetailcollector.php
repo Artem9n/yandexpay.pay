31a14b71417eff46fa7b10865b217247fc47231e
Korchebny <artem9n24@yandex.ru>
2022-05-18 16:38:23 +0800

    #216430 purchase rest: change pickup detail collect

--- lib/trading/action/rest/pickupdetail/stage/pickupdetailcollector.php
+++ lib/trading/action/rest/pickupdetail/stage/pickupdetailcollector.php
@@ -2,11 +2,12 @@
 namespace YandexPay\Pay\Trading\Action\Rest\PickupDetail\Stage;
 
 use YandexPay\Pay\Trading\Action\Rest\PickupDetail;
-use YandexPay\Pay\Trading\Action\Rest\PickupOptions\Stage;
 use YandexPay\Pay\Trading\Action\Rest\Reference\EffectiveResponse;
+use YandexPay\Pay\Trading\Action\Rest\Stage\ResponseCollector;
 use YandexPay\Pay\Trading\Action\Rest\State;
+use YandexPay\Pay\Trading\Entity\Reference as EntityReference;
 
-class PickupDetailCollector extends Stage\PickupOptionsCollector
+class PickupDetailCollector extends ResponseCollector
 {
 	protected $request;
 
@@ -29,6 +30,25 @@ class PickupDetailCollector extends Stage\PickupOptionsCollector
 		$result = $this->collectPickupOption($calculationResult, $store, $this->request->getLocationId());
 
 		$this->write($result);
+	}
+
+	protected function collectPickupOption(EntityReference\Delivery\CalculationResult $calculationResult, array $store, int $locationId = null) : array
+	{
+		return [
+			'pickupPointId' => implode(':', [$calculationResult->getDeliveryId(), $store['ID'], $locationId]), //todo send need metadata
+			'provider' => 'IN_STORE', //todo enum<PICKPOINT|...|IN_STORE>
+			'address' => $store['ADDRESS'],
+			'location' =>  [
+				'latitude' => (float)$store['GPS_N'],
+				'longitude' => (float)$store['GPS_S'],
+			],
+			'title'     => $store['TITLE'],
+			'fromDate' => $calculationResult->getDateFrom()->format('Y-m-d'),
+			'toDate' => $calculationResult->getDateTo()->format('Y-m-d'),
+			'amount' => (float)$calculationResult->getPrice(),
+			'description' => $store['DESCRIPTION'],
+			'phones' => explode(', ', $store['PHONE']),
+		];
 	}
 }
 
