f05be971ed3ff05ca1d1267eb2c46cd78fd47c08
Korchebny <artem9n24@yandex.ru>
2022-05-19 16:02:02 +0800

    #229774 subscriber event proxy

--- install/js/widget/ui/nodepreserver/subscriber.js
+++ install/js/widget/ui/nodepreserver/subscriber.js
@@ -1,7 +1,10 @@
+import {EventProxy} from "../../utils/eventproxy";
+
 export default class Subscriber {
 
 	static defaults = {
 		check: null,
+		event: null,
 		on: null,
 		off: null,
 	}
@@ -21,21 +24,61 @@ export default class Subscriber {
 	}
 
 	bind() {
-		if (this.options.on == null) {
-			console?.warn('define "on" option for subscriber of node preserver');
-			return;
-		}
+		this.bindOn();
+		this.bindEvent();
+	}
+
+	unbind() {
+		this.unbindOff();
+		this.unbindEvent();
+	}
+
+	bindOn() {
+		if (this.options.on == null) { return; }
 
 		this.options.on(this.options.check);
 	}
 
-	unbind() {
-		if (this.options.off == null) {
-			console?.warn('define "off" option for subscriber of node preserver');
-			return;
-		}
+	unbindOff() {
+		if (this.options.off == null) { return; }
 
 		this.options.off(this.options.check);
+	}
+
+	bindEvent() {
+		const event = this.options.event;
+
+		if (event == null) { return; }
+
+		const proxy = new EventProxy();
+
+		if (typeof event === 'string') {
+			proxy.on(event, this.options.check);
+		} else if (Array.isArray(event)) {
+			event.forEach((one) => {
+				proxy.on(one, this.options.check);
+			});
+		} else {
+			console?.warn(`unknown event type ${typeof event}`);
+		}
+	}
+
+	unbindEvent() {
+		const event = this.options.event;
+
+		if (event == null) { return; }
+
+		const proxy = new EventProxy();
+
+		if (typeof event === 'string') {
+			proxy.off(event, this.options.check);
+		} else if (Array.isArray(event)) {
+			event.forEach((one) => {
+				proxy.off(one, this.options.check);
+			});
+		} else {
+			console?.warn(`unknown event type ${typeof event}`);
+		}
 	}
 
 }
