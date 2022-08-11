361f52acfce74607977d501cbaa7cc29956d1ac7
Korchebny <artem9n24@yandex.ru>
2022-08-11 11:44:19 +0800

    #238239 yandex delivery: lang files

--- lang/ru/lib/delivery/yandex/handler.php
+++ lang/ru/lib/delivery/yandex/handler.php
@@ -1,5 +1,5 @@
 <?php
 
-$MESS['YANDEX_PAY_DELIVERY_YANDEX_HANDLER_TITLE'] = 'Служба доставки Yandex Pay Checkout';
-$MESS['YANDEX_PAY_DELIVERY_YANDEX_HANDLER_DESCRIPTION'] = 'Служба доставки Yandex Pay Checkout';
+$MESS['YANDEX_PAY_DELIVERY_YANDEX_HANDLER_TITLE'] = 'Яндекс.Доставка для Yandex Pay Checkout';
+$MESS['YANDEX_PAY_DELIVERY_YANDEX_HANDLER_DESCRIPTION'] = 'Яндекс.Доставка в форме оформления заказа Yandex Pay Checkout';
 $MESS['YANDEX_PAY_DELIVERY_YANDEX_HANDLER_CALC_ERROR'] = 'Стоимость доставки через данную службу не может быть посчитана. Попробуйте другую службу доставки.';
