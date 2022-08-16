94010c43aadd2298cfd8349742354104fa156d67
GordeyZhizhin <gordey_enot@mail.ru>
2022-08-16 13:13:08 +0800

    #241500 location suggest input

--- install/components/admin.field.warehouse/templates/.default/partials/suggest.php
+++ install/components/admin.field.warehouse/templates/.default/partials/suggest.php
@@ -4,26 +4,25 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) { die(); }
 //sale/install/components/bitrix/sale.location.selector.search/templates/.default/template.php
 
 ?>
-<div class="js-field-warehouse__suggest">
-	<div class="dropdown-block bx-ui-sls-input-block">
-
+<div class="bx-sls js-field-warehouse__suggest">
+	<div class="dropdown-block bx-ui-yapay-suggest-input-block">
 		<span class="dropdown-icon"></span>
 		<input type="text" autocomplete="off" class="dropdown-field" />
 
 		<div class="dropdown-fade2white"></div>
-		<div class="bx-ui-sls-loader"></div>
-		<div class="bx-ui-sls-clear" title=""></div>
-		<div class="bx-ui-sls-pane"></div>
+		<div class="bx-ui-yapay-suggest-loader"></div>
+		<div class="bx-ui-yapay-suggest-clear" title=""></div>
+		<div class="bx-ui-yapay-suggest-pane"></div>
 	</div>
-	<script type="text/html" data-template-id="bx-ui-sls-error">
-		<div class="bx-ui-sls-error">
+	<script type="text/html" data-template-id="bx-ui-yapay-suggest-error">
+		<div class="bx-ui-yapay-suggest-error">
 			<div></div>
 			{{message}}
 		</div>
 	</script>
 
-	<script type="text/html" data-template-id="bx-ui-sls-dropdown-item">
-		<div class="dropdown-item bx-ui-sls-variant">
+	<script type="text/html" data-template-id="bx-ui-yapay-suggest-dropdown-item">
+		<div class="dropdown-item bx-ui-yapay-suggest-variant">
 			<span class="dropdown-item-text">{{display_wrapped}}</span>
 		</div>
 	</script>
--- install/components/admin.field.warehouse/templates/.default/src/suggestwidget.js
+++ install/components/admin.field.warehouse/templates/.default/src/suggestwidget.js
@@ -1,9 +1,16 @@
+import "./suggestwidget.css";
+
 export class SuggestWidget extends BX.ui.autoComplete {
 
 	constructor(options) {
 		super(options.widget);
+
 		this.onSelect = options.onSelect;
 		this.search = options.search;
+	}
+	handleInitStack(nf, owner, opts) {
+		this.sys.code = 'yapay-suggest';
+		super.handleInitStack(nf, owner, opts);
 	}
 
 	downloadBundle(request, onLoad, onComplete, onError) {
