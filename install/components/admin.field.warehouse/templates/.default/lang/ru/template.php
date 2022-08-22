ee0588716eab06a177f7f5eb7bb8f7882e419112
GordeyZhizhin <gordey_enot@mail.ru>
2022-08-22 16:18:40 +0800

    #241500 edit messages

--- install/components/admin.field.warehouse/templates/.default/lang/ru/template.php
+++ install/components/admin.field.warehouse/templates/.default/lang/ru/template.php
@@ -1,7 +1,7 @@
 <?php
 
 $MESS['YAPAY_FIELD_WAREHOUSE_SUGGEST_ERROR'] = 'Ошибка';
-$MESS['YAPAY_FIELD_WAREHOUSE_SUGGEST_MAP_ERROR'] = 'Ошибка карт<br> Убедитесь, что в настройках модуля "Управление структурой" задан верный ключ API Yandex Карт';
-$MESS['YAPAY_FIELD_WAREHOUSE_CANT_LOAD_MAPS'] = 'Ошибка<br> Не удалось загрузить Яндекс.Карты';
-$MESS['YAPAY_FIELD_WAREHOUSE_MAPS_API_KEY_NOT_FOUND'] = 'Ошибка<br> Не задан ключ API Yandex Карт в настройках модуля "Управление структурой"';
+$MESS['YAPAY_FIELD_WAREHOUSE_SUGGEST_MAP_ERROR'] = '<b>Ошибка</b><br> Убедитесь, что задан верный ключ API Yandex Карт в <a href="/bitrix/admin/settings.php?lang=ru&mid=fileman">настройках модуля &laquo;Управление структурой&raquo;</a>';
+$MESS['YAPAY_FIELD_WAREHOUSE_CANT_LOAD_MAPS'] = '<b>Ошибка</b><br> Не удалось загрузить Яндекс.Карты';
+$MESS['YAPAY_FIELD_WAREHOUSE_MAPS_API_KEY_NOT_FOUND'] = '<b>Ошибка</b><br> Не задан ключ API Yandex Карт <a href="/bitrix/admin/settings.php?lang=ru&mid=fileman">настройках модуля &laquo;Управление структурой&raquo;</a>';
 $MESS['YAPAY_FIELD_WAREHOUSE_SUGGEST_NOTING_FOUND'] = 'Ничего не найдено';
