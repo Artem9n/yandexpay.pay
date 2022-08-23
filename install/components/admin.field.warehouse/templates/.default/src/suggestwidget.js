54ad4c04cef60002c7bc30cf0eec6dc647f87b0e
Petr Mishakov <mishakovpetr@ya.ru>
2022-08-23 14:34:23 +0800

    #241500 review misc

--- install/components/admin.field.warehouse/templates/.default/src/suggestwidget.js
+++ install/components/admin.field.warehouse/templates/.default/src/suggestwidget.js
@@ -25,11 +25,12 @@ export class SuggestWidget extends BX.ui.autoComplete {
 		this.setOptions();
 		super.handleInitStack(nf, owner, opts);
 	}
+
 	setOptions() {
 		BX.merge(this, {
 			opts: {
 				messages: {
-					nothingFound: BX.message('YAPAY_FIELD_WAREHOUSE_SUGGEST_NOTING_FOUND'),
+					nothingFound: BX.message('YAPAY_FIELD_WAREHOUSE_SUGGEST_NOTHING_FOUND'),
 					error: BX.message('YAPAY_FIELD_WAREHOUSE_SUGGEST_ERROR'),
 				},
 				bindEvents: {
@@ -46,7 +47,6 @@ export class SuggestWidget extends BX.ui.autoComplete {
 		this.search(request, onLoad, onComplete, onError);
 	}
 
-	// invokes when user selects value
 	selectItem = (value) => {
 		super.selectItem(value);
 		this.onSelect(value);
@@ -55,6 +55,7 @@ export class SuggestWidget extends BX.ui.autoComplete {
 	showLoading() {
 		this.vars.loader.show();
 	}
+
 	hideLoading() {
 		this.vars.loader.hide();
 	}
