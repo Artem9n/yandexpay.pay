cc53363b97ba306e012491d2804c7d542ed7df52
Korchebny <artem9n24@yandex.ru>
2022-08-31 16:37:59 +0800

    #238239 yandex delivery: change warehouse, user types on registration for the event

--- lib/ui/userfield/usertype.php
+++ lib/ui/userfield/usertype.php
@@ -3,9 +3,24 @@
 namespace YandexPay\Pay\Ui\UserField;
 
 use Bitrix\Main;
+use YandexPay\Pay\Reference\Concerns;
 
 class UserType
 {
+	use Concerns\HasMessage;
+
+	public const USER_TYPE_ID = 'yapay_user';
+
+	public function getUserTypeDescription() : array
+	{
+		return [
+			'DESCRIPTION' => static::getMessage('NAME'),
+			'USER_TYPE_ID' => static::USER_TYPE_ID,
+			'CLASS_NAME' => static::class,
+			'BASE_TYPE' => \CUserTypeManager::BASE_TYPE_INT,
+		];
+	}
+
 	public static function getEditFormHTML($userField, $htmlControl) : string
 	{
 		Main\UI\Extension::load('yandexpaypay.admin.ui.usertype');
@@ -14,19 +29,36 @@ class UserType
 		$value = htmlspecialcharsbx($htmlControl['VALUE']);
 		$lang = LANGUAGE_ID;
 		$userData = static::getUserData($value);
+		$userHtml = '';
 		$login = $userData['LOGIN'] ?? '';
 		$firstName = $userData['FIRST_NAME'] ?? '';
 		$lastName = $userData['LAST_NAME'] ?? '';
+
+		if (!empty($userData))
+		{
+			$userHtml = sprintf('[<a target="_blank" class="tablebodylink" href="/bitrix/admin/user_edit.php?ID=%s&lang=%s">%s</a>] (%s) %s %s',
+				$value,
+				$lang,
+				$value,
+				$login,
+				$firstName,
+				$lastName
+			);
+		}
 
 		return <<<OUTPUT
 			<div class="yapay-user-type">
 				<input type="text" name="{$name}" id="{$name}" value="{$value}" size="5">
 				<button class="adm-btn js-plugin" type="button" data-plugin="Ui.UserType" data-url="/bitrix/admin/user_search.php?lang={$lang}">...</button>
-				<span class="yapay-user-data">
-					[<a target="_blank" class="tablebodylink" href="/bitrix/admin/user_edit.php?ID={$value}&lang={$lang}">{$value}</a>] ({$login}) {$firstName} {$lastName}
-				</span>
+				<span class="yapay-user-data">{$userHtml}</span>
 			</div>
 OUTPUT;
+	}
+
+	/** @noinspection PhpUnusedParameterInspection */
+	public function getDBColumnType(array $userField) : string
+	{
+		return 'int';
 	}
 
 	protected static function getUserData(string $id) : array
--- lib/ui/userfield/warehousetype.php
+++ lib/ui/userfield/warehousetype.php
@@ -1,11 +1,57 @@
 <?php
 namespace YandexPay\Pay\Ui\UserField;
 
+use YandexPay\Pay\Reference\Concerns;
+use YandexPay\Pay\Trading\Settings\Options;
+use YandexPay\Pay\Trading\Entity\Registry as EntityRegistry;
+
 /** @noinspection PhpUnused */
 class WarehouseType extends FieldsetType
 {
+	use Concerns\HasMessage;
+
+	public const USER_TYPE_ID = 'yapay_warehouse';
+
+	public function getUserTypeDescription() : array
+	{
+		return [
+			'DESCRIPTION' => static::getMessage('NAME'),
+			'USER_TYPE_ID' => static::USER_TYPE_ID,
+			'CLASS_NAME' => static::class,
+			'BASE_TYPE' => \CUserTypeManager::BASE_TYPE_STRING,
+		];
+	}
+
+	/** @noinspection PhpUnusedParameterInspection */
+	public function getDBColumnType(array $userField) : string
+	{
+		return 'text';
+	}
+
+	/** @noinspection PhpUnusedParameterInspection */
+	public static function onBeforeSave(array $userField, $value) : string
+	{
+		if (is_array($value))
+		{
+			$value = serialize($value);
+		}
+
+		return $value;
+	}
+
+	protected static function asSingle(array $userField, ?array $htmlControl)
+	{
+		if (!is_array($userField['VALUE']))
+		{
+			$userField['VALUE'] = unserialize($userField['VALUE'], [ 'allowed_classes' => false ]);
+		}
+
+		return parent::asSingle($userField, $htmlControl);
+	}
+
 	protected static function makeLayout($userField, $htmlControl) : Fieldset\AbstractLayout
 	{
+		$userField = static::userFieldDefaults($userField);
 		$fields = static::getFields($userField);
 
 		$result = new Fieldset\SummaryLayout($userField, $htmlControl['NAME'], $fields);
@@ -24,5 +70,32 @@ class WarehouseType extends FieldsetType
 		});
 
 		return $result;
+	}
+
+	protected static function userFieldDefaults(array $userField) : array
+	{
+		$userField['NAME'] = static::getMessage('NAME');
+
+		if (!isset($userField['SETTINGS'])) { $userField['SETTINGS'] = []; }
+
+		$userField['SETTINGS'] += [
+			'SUMMARY' => '#COUNTRY#, #LOCALITY#, #STREET#, #BUILDING#',
+			'LAYOUT' => 'summary',
+			'MODAL_WIDTH' => 800,
+			'MODAL_HEIGHT' => 600,
+		];
+
+		return $userField;
+	}
+
+	public static function getFields(array $userField) : array
+	{
+		if (isset($userField['FIELDS'])) { return $userField['FIELDS']; }
+
+		$option = new Options\Warehouse();
+		$environment = EntityRegistry::getEnvironment();
+		$siteId = $environment->getSite()->getDefault();
+
+		return $option->getFields($environment, $siteId);
 	}
 }
