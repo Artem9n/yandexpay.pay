0379072e81f650b5d86ec5a71e0c291adcdddfe8
Korchebny <artem9n24@yandex.ru>
2022-09-01 12:53:07 +0800

    #244512 update: fix ratio, type item count, fix related prop rest, fix refresh basket, status current lang

---
+++ updates/1.2.7/lib/trading/action/rest/ordercreate/stage/relatedproperties.php
@@ -0,0 +1,19 @@
+<?php
+namespace YandexPay\Pay\Trading\Action\Rest\OrderCreate\Stage;
+
+use YandexPay\Pay\Trading\Action\Rest\Utils;
+use YandexPay\Pay\Trading\Action\Rest\State;
+
+class RelatedProperties
+{
+	public function __invoke(State\OrderCalculation $state)
+	{
+		if (!empty($state->relatedProperties))
+		{
+			$state->order->fillProperties($state->relatedProperties);
+
+			$state->filledProperties += $state->relatedProperties;
+			$state->relatedProperties = [];
+		}
+	}
+}
