a41ddd273050577d89d79a0d8b7e9bd4e99b60e4
Korchebny <artem9n24@yandex.ru>
2022-09-16 16:14:07 +0800

    #244504 yandex delivery: collector schedule

--- lib/trading/action/rest/stage/yandexdeliverycollector.php
+++ lib/trading/action/rest/stage/yandexdeliverycollector.php
@@ -38,6 +38,7 @@ class YandexDeliveryCollector extends Rest\Stage\ResponseCollector
 			{
 				$contactId = $this->yandexDelivery->getEmergencyContact();
 				$warehouse = $this->yandexDelivery->getWarehouse();
+				$schedule = $this->yandexDelivery->getShipmentSchedule();
 			}
 			else
 			{
@@ -45,23 +46,28 @@ class YandexDeliveryCollector extends Rest\Stage\ResponseCollector
 
 				$storeWarehouseField = $this->yandexDelivery->getStoreWarehouseField();
 				$storeContactField = $this->yandexDelivery->getStoreContactField();
+				$storeScheduleField = $this->yandexDelivery->getStoreShipmentSchedule();
 
 				$storeIds = $storeService->available($state->order);
 
 				if (empty($storeIds)) { return; }
 
-				$storeId = $storeService->expressStrategy($strategy)->resolve($storeIds, $this->address, [
+				$strategyService = $storeService->expressStrategy($strategy);
+
+				$store = $strategyService->resolve($storeIds, $this->address, [
 					'WAREHOUSE_FIELD' => $storeWarehouseField,
 					'CONTACT_FIELD' => $storeContactField,
+					'SHIPMENT_SCHEDULE_FIELD' => $storeScheduleField,
 				]);
 
-				if ($storeId === null)
+				if ($store === null)
 				{
 					throw new Main\SystemException('not resolve store');
 				}
 
-				$contactId = $storeService->contact($storeId, $storeContactField);
-				$warehouse = $storeService->warehouse($storeId, $storeWarehouseField);
+				$contactId = $storeService->contact($store, $storeContactField);
+				$warehouse = $storeService->warehouse($store, $storeWarehouseField);
+				$schedule = $storeService->schedule($store, $storeScheduleField);
 			}
 
 			$contact = $this->contact($state, $contactId);
@@ -70,6 +76,7 @@ class YandexDeliveryCollector extends Rest\Stage\ResponseCollector
 				'contact' => $contact,
 				'emergencyContact' => $contact,
 				'address' => $this->warehouseAddress($warehouse),
+				'schedule' => $this->scheduleCollector($schedule),
 			]);
 		}
 		catch (Main\SystemException $exception)
@@ -111,5 +118,104 @@ class YandexDeliveryCollector extends Rest\Stage\ResponseCollector
 				'latitude' => $warehouse->getLat(),
 			],
 		];
+	}
+
+	protected function scheduleCollector(Options\ShipmentSchedule $schedule) : array
+	{
+		$weekly = $this->weeklyCollector($schedule->getSchedule());
+		$custom = $this->customCollector($schedule->getHoliday());
+
+		return [
+			'tzoffset' => $schedule->getTimeZoneOffset(),
+			'weekly' => $weekly,
+			'custom' => $custom,
+		];
+	}
+
+	protected function weeklyCollector(Options\ScheduleOptions $options) : array
+	{
+		$result = [];
+		$weekdays = [
+			1 => 'mon',
+			2 => 'tue',
+			3 => 'wed',
+			4 => 'thu',
+			5 => 'fri',
+			6 => 'sat',
+			7 => 'sun',
+		];
+
+		for ($i = 1; $i <= 7; ++$i)
+		{
+			$workTime = null;
+
+			/** @var Options\ScheduleOption $workday */
+			foreach ($options as $workday)
+			{
+				if ($workday->isMatchWeekday($i))
+				{
+					$startTime = $workday->getFromTime();
+					$endTime = $workday->getToTime();
+
+					if ($startTime !== null && $endTime !== null)
+					{
+						$workTime = [
+							'start' => $workday->getFromTime(),
+							'end' => $workday->getToTime(),
+						];
+					}
+					break;
+				}
+			}
+
+			$day = $weekdays[$i];
+			$result[$day] = $workTime;
+		}
+
+		return $result;
+	}
+
+	protected function customCollector(Options\HolidayOption $holiday) : array
+	{
+		$result = [];
+		$calendar = $holiday->getCalendar();
+		$holidays = array_flip($calendar->holidays());
+		$workdays = array_flip($calendar->workdays());
+		$holidayWorktime = $holiday->getIntervals();
+		$iteratorDate = new Main\Type\Date();
+
+		for ($i = 0; $i < 30; ++$i)
+		{
+			$day = $iteratorDate->format('d.m');
+
+			if (isset($workdays[$day]))
+			{
+				$startTime = $holidayWorktime->getFromTime();
+				$endTime = $holidayWorktime->getToTime();
+				$workTime = null;
+				if ($startTime !== null && $endTime !== null)
+				{
+					$workTime = [
+						'start' => $holidayWorktime->getFromTime(),
+						'end' => $holidayWorktime->getToTime(),
+					];
+				}
+			}
+			else if (isset($holidays[$day]))
+			{
+				$workTime = null;
+			}
+			else
+			{
+				$iteratorDate->add('P1D');
+				continue;
+			}
+
+			$result[$iteratorDate->format('Y-m-d')] = $workTime;
+
+			$iteratorDate->add('P1D');
+		}
+
+		return $result;
 	}
 }
