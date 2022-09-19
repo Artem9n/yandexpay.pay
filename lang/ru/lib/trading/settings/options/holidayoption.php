46a108961be6c90caef89b801e0440c0bb6a459d
Korchebny <artem9n24@yandex.ru>
2022-09-19 10:52:54 +0800

    #244504 yandex delivery: fix status coupons, fix lang holiday option

--- lang/ru/lib/trading/settings/options/holidayoption.php
+++ lang/ru/lib/trading/settings/options/holidayoption.php
@@ -4,7 +4,10 @@ $MESS['YANDEX_PAY_TRADING_SETTINGS_OPTIONS_HOLIDAY_OPTION_CALENDAR'] = 'Календар
 $MESS['YANDEX_PAY_TRADING_SETTINGS_OPTIONS_HOLIDAY_OPTION_INTERVALS'] = 'Время работы';
 $MESS['YANDEX_PAY_TRADING_SETTINGS_OPTIONS_HOLIDAY_OPTION_HOLIDAYS'] = 'Выходные дни';
 $MESS['YANDEX_PAY_TRADING_SETTINGS_OPTIONS_HOLIDAY_OPTION_WORKDAYS'] = 'Рабочие дни';
-$MESS['YANDEX_PAY_TRADING_SETTINGS_OPTIONS_HOLIDAY_OPTION_CALENDAR_HELP'] = 'Выберите один из календарей, согласно которого ваш склад для доставки отдыхает.';
+$MESS['YANDEX_PAY_TRADING_SETTINGS_OPTIONS_HOLIDAY_OPTION_CALENDAR_HELP'] = 'Выберите один из календарей, согласно которого отгрузка не выполняется.
+<br><br><strong>Производственный календарь</strong> - Выходные дни: 06.03, 07.03, 08.03, 30.04, 01.05, 02.05, 03.05, 07.05, 08.05, 09.05, 10.05, 11.06, 12.06, 13.06, 04.11
+<br>Предпраздничные дни - 22.02, 05.03, 03.11
+<br><br><strong>Государственные праздники</strong> - Выходные дни: 01.01, 02.01, 03.01, 04.01, 05.01, 06.01, 07.01, 08.01, 23.02, 08.03, 01.05, 09.05, 12.06, 04.11, 31.12';
 $MESS['YANDEX_PAY_TRADING_SETTINGS_OPTIONS_HOLIDAY_OPTION_INTERVALS_HELP'] = 'График доставки в рабочие дни.';
 $MESS['YANDEX_PAY_TRADING_SETTINGS_OPTIONS_HOLIDAY_OPTION_HOLIDAYS_HELP'] = 'Укажите дни, по которым не работает ваш склад.';
 $MESS['YANDEX_PAY_TRADING_SETTINGS_OPTIONS_HOLIDAY_OPTION_WORKDAYS_HELP'] = 'Укажите те выходные дни, по которым работает ваш склад. Например, если вы готовы отгрузить товар 01.01 — укажите этот день тут.';
