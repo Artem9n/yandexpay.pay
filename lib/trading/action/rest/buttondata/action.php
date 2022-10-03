a9dfa48309bb08c853ee83f04332ca194dc36c69
Korchebny <artem9n24@yandex.ru>
2022-10-03 10:43:42 +0800

    #244511 change action button data

--- lib/trading/action/rest/buttondata/action.php
+++ lib/trading/action/rest/buttondata/action.php
@@ -6,38 +6,36 @@ use YandexPay\Pay\Trading\Action\Rest;
 
 class Action extends Rest\Reference\EffectiveAction
 {
+	/** @var Request */
+	protected $request;
+
 	public function bootstrap() : void
 	{
 		$this->bootJson();
-		$this->bootSetup();
-	}
-
-	protected function getSetupId() : int
-	{
-		return $this->httpRequest->get('setupId');
+		$this->request = $this->convertHttpToRequest(Request::class);
+		$this->bootSetup($this->request->getSetupId());
 	}
 
 	public function process() : Main\HttpResponse
 	{
-		$request = $this->convertHttpToRequest(Request::class);
 		$response = $this->makeResponse();
 		$state = $this->makeState(Rest\State\OrderCalculation::class);
 
 		(new Rest\Pipeline())
-			->pipe($this->calculationPipeline($request))
+			->pipe($this->calculationPipeline())
 			->pipe($this->collectorPipeline($response))
-			->pipe(new Rest\ButtonData\Stage\MetaCollector($response, $request, 'metadata'))
+			->pipe(new Rest\ButtonData\Stage\MetaCollector($response, $this->request, 'metadata'))
 			->process($state);
 
 		return $this->convertResponseToHttp($response);
 	}
 
-	protected function calculationPipeline(Request $request) : Rest\Pipeline
+	protected function calculationPipeline() : Rest\Pipeline
 	{
 		return (new Rest\Pipeline())
 			->pipe(new Rest\ButtonData\Stage\SearchUser())
 			->pipe(new Rest\Stage\NewOrder())
-			->pipe(new Rest\ButtonData\Stage\WakeUpBasket($request->getMode(), $request->getProductId()));
+			->pipe(new Rest\ButtonData\Stage\WakeUpBasket($this->request->getMode(), $this->request->getProductId()));
 	}
 
 	protected function collectorPipeline(Rest\Reference\EffectiveResponse $response) : Rest\Pipeline
