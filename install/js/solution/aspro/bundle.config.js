8fee224523f4bc0706bf4c38a44be94fb3293d3d
Korchebny <artem9n24@yandex.ru>
2022-11-08 13:10:03 +0800

    #250318 add common solution aspro js

--- install/js/solution/aspro/bundle.config.js
+++ install/js/solution/aspro/bundle.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	input: './index.js',
 	output: './build/solution.js',
-	namespace: 'BX.YandexPay.Solution.AsproMax',
+	namespace: 'BX.YandexPay.Solution.Aspro',
 	adjustConfigPhp: false,
 	browserslist: true,
 	minification: true,
