3b94a73813b381bfe080ccb1505df7765b6fa883
Korchebny <artem9n24@yandex.ru>
2022-11-10 13:29:01 +0800

    #250318 lang files

--- lang/ru/lib/injection/behavior/basket.php
+++ lang/ru/lib/injection/behavior/basket.php
@@ -1,5 +1,5 @@
 <?php
 
 $MESS['YANDEX_PAY_INJECTION_BEHAVIOR_BASKET_TITLE'] = 'Корзина товаров';
-$MESS['YANDEX_PAY_INJECTION_BEHAVIOR_BASKET_SELECTOR'] = 'Селектор, куда привязывать кнопку "Yandex Pay"';
 $MESS['YANDEX_PAY_INJECTION_BEHAVIOR_BASKET_PATH'] = 'Путь к странице с корзиной от корня сайта';
+$MESS['YANDEX_PAY_INJECTION_BEHAVIOR_BASKET_GROUP_BEHAVIOR'] = 'Правило вставки';
