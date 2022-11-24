69c637360a523e20151dae9f1f02c084039efe75
Korchebny <artem9n24@yandex.ru>
2022-11-24 16:18:53 +0800

    #255542 composite cache environment

---
+++ lib/trading/entity/common/composite/clear.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace YandexPay\Pay\Trading\Entity\Common\Composite;
+
+use Bitrix\Main;
+use YandexPay\Pay\Reference;
+
+class Clear extends Reference\Agent\Base
+{
+	public static function clear(string $domain) : bool
+	{
+		$query = Main\Composite\Internals\Model\PageTable::getList([
+			'filter' => [
+				'HOST' => $domain,
+			],
+			'select' => [ 'ID', 'URI']
+		]);
+
+		while ($page = $query->fetch())
+		{
+			$composite = new Main\Composite\Page($page['URI'], $domain);
+			$composite->delete();
+		}
+
+		return false;
+	}
+}
