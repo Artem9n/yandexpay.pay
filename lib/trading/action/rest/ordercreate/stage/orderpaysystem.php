2e949a3e023ef4bd69b7a1bba38e6eb5485d3f14
Korchebny <artem9n24@yandex.ru>
2022-12-27 17:11:57 +0800

    #260592 options property id for check payment split

--- lib/trading/action/rest/ordercreate/stage/orderpaysystem.php
+++ lib/trading/action/rest/ordercreate/stage/orderpaysystem.php
@@ -1,6 +1,7 @@
 <?php
 namespace YandexPay\Pay\Trading\Action\Rest\OrderCreate\Stage;
 
+use YandexPay\Pay\Config;
 use YandexPay\Pay\Reference\Concerns;
 use YandexPay\Pay\Trading\Action\Rest\OrderCreate\Request;
 use YandexPay\Pay\Trading\Action\Rest\State;
@@ -33,6 +34,8 @@ class OrderPaySystem
                 'PAY_SYSTEM_NAME' => self::getMessage('SPLIT_NAME'),
                 'COMMENTS' => self::getMessage('SPLIT_COMMENTS'),
             ];
+
+			$this->fillPropertySplit($state);
 		}
 		else if ($paymentType === 'CASH_ON_DELIVERY')
 		{
@@ -43,5 +46,16 @@ class OrderPaySystem
 		{
 			$state->order->createPayment($paySystemId, null, $data);
 		}
+	}
+
+	protected function fillPropertySplit(State\OrderCalculation $state) : void
+	{
+		$propertyIdSplit = Config::getOption('order_property_id_split', null);
+
+		if ($propertyIdSplit === null) { return; }
+
+		$state->order->fillProperties([
+			$propertyIdSplit => self::getMessage('SPLIT_COMMENTS'),
+		]);
 	}
 }
