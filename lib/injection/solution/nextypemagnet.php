124ca6ddad8c99b2570aa5f1dfba0bdc226a6d9c
Darina Astashkevich <stah99@mail.ru>
2023-01-20 18:12:16 +0800

    #262763 текущие наработки basket fly/fast view

--- lib/injection/solution/nextypemagnet.php
+++ lib/injection/solution/nextypemagnet.php
@@ -28,37 +28,94 @@ class NextypeMagnet extends Skeleton
 		return Guide::getBitrixOrderPath($context, '/order/');
 	}
 
-	public function getDefaults(array $context = []) : array
+	protected function designDefaults() : array
 	{
 		return [
-			Behavior\Registry::ELEMENT => [
+			'DISPLAY' => Behavior\Display\Registry::BUTTON,
+			'HEIGHT_TYPE_BUTTON' => 'OWN',
+			'BORDER_RADIUS_TYPE_BUTTON' => 'OWN',
+			'BORDER_RADIUS_VALUE_BUTTON' => 30,
+			'WIDTH_BUTTON' => 'MAX',
+		];
+	}
+
+	protected function basketFlyDefaults(array $context = []) : array
+	{
+		$paths = [
+			'/ajax/basket_fly.php',
+			'/ajax/showBasketHover.php',
+		];
+
+		foreach ($paths as &$path)
+		{
+			$dir = $context['SITE_DIR'] ?? '';
+			$dir = rtrim($dir, '/');
+			$path = $dir . $path;
+		}
+		unset($path);
+
+		$settings = [
+			'SELECTOR' => '.header-cart.fly .basket_back, .basket_hover_block .basket_wrap .buttons',
+			'USE_DIVIDER' => false,
+			'WIDTH_BUTTON' => 'MAX',
+			'PATH' => implode(PHP_EOL, $paths),
+			'POSITION' => 'afterend',
+		];
+
+		$design = $this->designDefaults();
+
+		return $settings + $design;
+	}
+
+	protected function elementDefaults(array $context = []) : array
+	{
+		return [
 				'SELECTOR' => '.product-main-info .info .order-container .buttons .product-item-button-container div',
 				'POSITION' => 'afterend',
 				'IBLOCK' => $context['IBLOCK'],
-				'DISPLAY' => Behavior\Display\Registry::BUTTON,
-				'WIDTH_BUTTON' => 'MAX',
-				'HEIGHT_TYPE_BUTTON' => 'OWN',
 				'HEIGHT_VALUE_BUTTON' => 42,
-				'BORDER_RADIUS_TYPE_BUTTON' => 'OWN',
-				'BORDER_RADIUS_VALUE_BUTTON' => 30,
-			],
-			Behavior\Registry::BASKET => [
-					'HEIGHT_TYPE_BUTTON' => 'OWN',
-					'HEIGHT_VALUE_BUTTON' => 44,
-					'BORDER_RADIUS_TYPE_BUTTON' => 'OWN',
-					'BORDER_RADIUS_VALUE_BUTTON' => 30,
-					'WIDTH_BUTTON' => 'MAX',
-				]
-				+ Guide::getBitrixBasket($context),
-			Behavior\Registry::ORDER => [
-					'HEIGHT_TYPE_BUTTON' => 'OWN',
-					'HEIGHT_VALUE_BUTTON' => 52,
-					'BORDER_RADIUS_TYPE_BUTTON' => 'OWN',
-					'BORDER_RADIUS_VALUE_BUTTON' => 30,
-					'WIDTH_BUTTON' => 'MAX',
-					'POSITION' => 'afterend',
-				]
-				+ Guide::getBitrixOrder($context),
+			] + $this->designDefaults();
+	}
+
+	protected function basketDefaults(array $context = []) : array
+	{
+		$design = [
+			'HEIGHT_VALUE_BUTTON' => 44,
+		] + $this->designDefaults();
+
+		$settings = Guide::getBitrixBasket($context);
+
+		return $design + $settings;
+	}
+
+	protected function orderDefaults(array $context = []) : array
+	{
+		$design = [
+			'HEIGHT_VALUE_BUTTON' => 52,
+			'POSITION' => 'afterend',
+			] + $this->designDefaults();
+
+		$settings = Guide::getBitrixOrder($context);
+
+		return $design + $settings;
+	}
+
+	protected function elementFastDefaults(array $context = []) : array
+	{
+		$elementSettings = $this->elementDefaults($context);
+		$elementSettings['QUERY_CHECK_PARAMS'] = 'FAST_VIEW=Y';
+
+		return $elementSettings;
+	}
+
+	public function getDefaults(array $context = []) : array
+	{
+		return [
+			Behavior\Registry::ELEMENT => $this->elementDefaults($context),
+			Behavior\Registry::BASKET => $this->basketDefaults($context),
+			Behavior\Registry::ORDER => $this->orderDefaults($context),
+			Behavior\Registry::BASKET_FLY => $this->basketFlyDefaults($context),
+			Behavior\Registry::ELEMENT_FAST => $this->elementFastDefaults($context),
 		];
 	}
 }
