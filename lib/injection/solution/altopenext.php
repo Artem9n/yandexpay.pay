997a4ddc29a7990225f08692896afc86552f7e25
Darina Astashkevich <stah99@mail.ru>
2023-01-26 16:37:39 +0800

    #262769 адаптация для решения "ЭЛЕКТРОСИЛА NEXT - Широкоформатный интернет-магазин, Маркетплейс, Агрегатор товаров" - карточка, корзина, оформление заказа

--- lib/injection/solution/altopenext.php
+++ lib/injection/solution/altopenext.php
@@ -28,38 +28,61 @@ class AltopEnext extends Skeleton
 		return Guide::getBitrixOrderPath($context);
 	}
 
+	protected function designDefaults() : array
+	{
+		return [
+			'DISPLAY' => Behavior\Display\Registry::BUTTON,
+			'HEIGHT_TYPE_BUTTON' => 'OWN',
+			'BORDER_RADIUS_TYPE_BUTTON' => 'OWN',
+			'BORDER_RADIUS_VALUE_BUTTON' => 4,
+			'WIDTH_BUTTON' => 'MAX',
+		];
+	}
+
+	protected function elementDefaults(array $context = []) : array
+	{
+		return [
+			'SELECTOR' => '.product-item-detail-pay-block .product-item-detail-button-container',
+			'POSITION' => 'afterend',
+			'IBLOCK' => 29,
+			'HEIGHT_VALUE_BUTTON' => 48,
+		] + $this->designDefaults();
+	}
+
+	protected function basketDefaults(array $context = []) : array
+	{
+		$design = [
+				'SELECTOR' => '.bx_ordercart_order_pay .bx_ordercart_order_pay_right',
+				'HEIGHT_TYPE_BUTTON' => 'OWN',
+				'HEIGHT_VALUE_BUTTON' => 42,
+				'POSITION' => 'beforeend',
+			] + $this->designDefaults();
+
+		$settings = Guide::getBitrixBasket($context);
+
+		return $design + $settings;
+	}
+
+	protected function orderDefaults(array $context = []) : array
+	{
+		$design = [
+				'HEIGHT_TYPE_BUTTON' => 'OWN',
+				'HEIGHT_VALUE_BUTTON' => 46,
+				'POSITION' => 'beforeend',
+			] + $this->designDefaults();
+
+		$settings = Guide::getBitrixOrder($context);
+
+		return $design + $settings;
+	}
+
+
 	public function getDefaults(array $context = []) : array
 	{
 		return [
-			Behavior\Registry::ELEMENT => [
-				'SELECTOR' => '.product-item-detail-pay-block .product-item-detail-button-container',
-				'POSITION' => 'afterend',
-				'IBLOCK' => 29,
-				'DISPLAY' => Behavior\Display\Registry::BUTTON,
-				'WIDTH_BUTTON' => 'MAX',
-				'HEIGHT_TYPE_BUTTON' => 'OWN',
-				'HEIGHT_VALUE_BUTTON' => 48,
-				'BORDER_RADIUS_TYPE_BUTTON' => 'OWN',
-				'BORDER_RADIUS_VALUE_BUTTON' => 4,
-			],
-			Behavior\Registry::BASKET => [
-					'HEIGHT_TYPE_BUTTON' => 'OWN',
-					'HEIGHT_VALUE_BUTTON' => 42,
-					'BORDER_RADIUS_TYPE_BUTTON' => 'OWN',
-					'BORDER_RADIUS_VALUE_BUTTON' => 4,
-					'WIDTH_BUTTON' => 'MAX',
-				]
-				+ Guide::getBitrixBasket($context),
-			Behavior\Registry::ORDER => [
-					'HEIGHT_TYPE_BUTTON' => 'OWN',
-					'HEIGHT_VALUE_BUTTON' => 46,
-					'BORDER_RADIUS_TYPE_BUTTON' => 'OWN',
-					'BORDER_RADIUS_VALUE_BUTTON' => 4,
-					'WIDTH_BUTTON' => 'MAX',
-					'POSITION' => 'afterend',
-				]
-				+ Guide::getBitrixOrder($context),
-
+			Behavior\Registry::ELEMENT => $this->elementDefaults($context),
+			Behavior\Registry::BASKET => $this->basketDefaults($context),
+			Behavior\Registry::ORDER => $this->orderDefaults($context)
 		];
 	}
 }
