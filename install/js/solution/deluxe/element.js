7da60178423f99e6df4fd0cb9d3efd15ef56487f
Korchebny <artem9n24@yandex.ru>
2023-02-02 14:27:25 +0800

    #263348 deluxe: select first offer, change select offer

--- install/js/solution/deluxe/element.js
+++ install/js/solution/deluxe/element.js
@@ -2,26 +2,58 @@ import Page from "../reference/page";
 
 export default class Element extends Page {
 
+	initialHandler;
+
 	bootCart(cart) {
 		this.cart = cart;
+		this.onStarterOffer();
 		this.onCommonOffer();
 	}
 
+	onStarterOffer() {
+		if (window?.elementStoresComponentParams == null) { return; }
+
+		const firstOfferId = parseInt(window?.elementStoresComponentParams?.OFFER_ID, 10);
+
+		if (isNaN(firstOfferId)) { return; }
+
+		this.cart.changeOffer(firstOfferId);
+	}
+
 	onCommonOffer() {
-		$(document).ajaxComplete(( event, xhr, settings ) => {
+
+		if (this.initialHandler != null) { return; }
+
+		$(document).ajaxSuccess(( event, request, settings, data ) => {
 			if (
 				settings.url.match(/(\/catalog.item\/)(.*)(\/ajax.php)/) != null
 				|| settings.url.indexOf('act=selectSku') !== -1
 			) {
-				const response = xhr.responseJSON;
+				const response = data;
 
 				if (typeof response !== "object") { return; }
 
-				const newProductId = response[0]?.PRODUCT?.ID;
+				let newProductId;
 
-				if (newProductId == null) { return; }
+				for (let key in response) {
 
-				this.initialProduct = newProductId;
+					if (!response.hasOwnProperty(key)) { continue; }
+
+					for (let entity in response[key]) {
+
+						if (!response[key].hasOwnProperty(entity)) { continue; }
+
+						if (entity !== 'PRODUCT') { continue; }
+
+						newProductId = parseInt(response[key].PRODUCT?.ID, 10);
+
+						if (!isNaN(newProductId)) { break; }
+					}
+				}
+
+				if (newProductId == null || isNaN(newProductId)) { return; }
+
+				this.initialHandler = true;
 
 				this.cart.delayChangeOffer(newProductId);
 			}
