6a644cfbac74d010743e8bec3466aa7badf5c7bb
Korchebny <artem9n24@yandex.ru>
2023-02-14 16:19:06 +0800

    #266245 nextypemagnet: change default settings js

--- install/js/solution/nextypemagnet/basket.js
+++ install/js/solution/nextypemagnet/basket.js
@@ -2,15 +2,4 @@ import EshopBasket from '../eshopbootstrap/basket';
 
 export default class Basket extends EshopBasket {
 
-	bootFactory(factory) {
-		super.bootFactory(factory);
-	}
-
-	bootCart(cart) {
-		if (typeof BX === 'undefined') { return; }
-
-		this.onEvent('OnBasketChange', () => {
-			cart.delayChangeBasket();
-		});
-	}
 }
