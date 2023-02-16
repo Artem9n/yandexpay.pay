43dbcd590baf2efe838db9ef51fc4b992a6667ca
Korchebny <artem9n24@yandex.ru>
2023-02-16 15:41:46 +0800

    #266245 remove console log

--- install/js/solution/aspro/max/elementfast.js
+++ install/js/solution/aspro/max/elementfast.js
@@ -6,8 +6,6 @@ export default class ElementFast extends Element {
 		const offerProp = document.querySelector(".fastview-product .to-cart:not(.read_more)");
 		const firstOfferId = parseInt(offerProp?.dataset?.item, 10);
 
-		console.log(firstOfferId);
-
 		if (isNaN(firstOfferId)) { return; }
 
 		this.cart.changeOffer(firstOfferId);
