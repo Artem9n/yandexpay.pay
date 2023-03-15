ab02ff96cc36733a50deab8a9efea82ec86ee292
Korchebny <artem9n24@yandex.ru>
2023-03-15 11:37:43 +0800

    #261449 save new display values description handler

---
+++ lib/ui/saleinput/mapping.php
@@ -0,0 +1,51 @@
+<?php
+namespace YandexPay\Pay\Ui\SaleInput;
+
+use Bitrix\Main;
+use Bitrix\Sale\BusinessValue;
+
+if (!Main\Loader::includeModule('sale')) { return; }
+
+class Mapping
+{
+	public static function saveMapping($codeKey, $personTypeId, $mapping) : Main\Result
+	{
+		$result = new Main\Result();
+
+		if ($codeKey === 'YANDEX_PAY_DISPLAY')
+		{
+			$mapping['PROVIDER_VALUE'] = static::formatDisplayFields($mapping['PROVIDER_VALUE']);
+		}
+
+		$request = Main\Application::getInstance()->getContext()->getRequest();
+		$consumerKey = null;
+
+		if ((int)$request->get('ID') > 0)
+		{
+			$consumerKey = 'PAYSYSTEM_'.$request->get('ID');
+		}
+
+		$common = !IsModuleInstalled('bitrix24');
+		BusinessValue::setMapping($codeKey, $consumerKey, $personTypeId, $mapping, $common);
+
+		return $result;
+	}
+
+	protected static function formatDisplayFields(array $fields) : string
+	{
+		$displayFields = [];
+		$display = mb_strtoupper($fields['DISPLAY']);
+
+		foreach ($fields as $key => $value)
+		{
+			if (
+				$key !== 'DISPLAY'
+				&& substr($key, strlen($key) - strlen($display)) !== $display
+			) { continue; }
+
+			$displayFields[$key] = $value;
+		}
+
+		return serialize($displayFields);
+	}
+}
