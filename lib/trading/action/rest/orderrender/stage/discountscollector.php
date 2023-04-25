03a8bd54146d81cf0849ba6691c2762c56c7b82d
Korchebny <artem9n24@yandex.ru>
2023-04-25 15:13:52 +0800

    #275822 the amount discounts for your handler

--- lib/trading/action/rest/orderrender/stage/discountscollector.php
+++ lib/trading/action/rest/orderrender/stage/discountscollector.php
@@ -1,7 +1,7 @@
 <?php
 namespace YandexPay\Pay\Trading\Action\Rest\OrderRender\Stage;
 
-use Bitrix\Sale;
+use Sale\Handlers;
 use YandexPay\Pay\Trading\Action\Rest\State;
 use YandexPay\Pay\Reference\Concerns;
 use YandexPay\Pay\Trading\Action\Rest\Stage\ResponseCollector;
@@ -14,9 +14,14 @@ class DiscountsCollector extends ResponseCollector
 	{
 		$result = [];
 
-		$discountList = $state->order->getDiscount()->getApplyResult();
-		$discountIds = array_column($discountList['DISCOUNT_LIST'], 'REAL_DISCOUNT_ID');
-		$discountAmount = $state->order->getPaymentCollection()->getPaidSum();
+		$discountIds = [];
+
+		$discountAmount = $this->sumPaid($state);
+
+		if ($discountAmount > 0)
+		{
+			$discountIds[] = 'paid';
+		}
 
 		/** @var \Bitrix\Sale\BasketItem $basketItem */
 		foreach ($state->order->getBasket() as $basketItem)
@@ -26,6 +31,9 @@ class DiscountsCollector extends ResponseCollector
 
 		if ($discountAmount <= 0) { return; }
 
+		$discountList = $state->order->getDiscount()->getApplyResult();
+		$discountIds = array_merge(array_column($discountList['DISCOUNT_LIST'], 'REAL_DISCOUNT_ID'), $discountIds);
+
 		$result[] = [
 			'amount' => (string)$discountAmount,
 			'description' => self::getMessage('DISCOUNT_DESCRIPTION'),
@@ -33,6 +41,23 @@ class DiscountsCollector extends ResponseCollector
 		];
 
 		$this->write($result, 'cart.discounts');
+	}
+
+	protected function sumPaid(State\Order $state) : float
+	{
+		$result = 0;
+
+		/** @var \Bitrix\Sale\Payment $payment */
+		foreach ($state->order->getPaymentCollection() as $payment)
+		{
+			$handler = $state->environment->getPaySystem()->getHandler($payment->getPaymentSystemId());
+
+			if ($handler instanceof Handlers\PaySystem\YandexPayHandler) { continue; }
+
+			$result += $payment->getSum();
+		}
+
+		return $result;
 	}
 }
 
